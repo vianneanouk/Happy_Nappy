db1d0e1523319c1170a7a1d762f7ac707be13653
Vianne Häfeli <vianne.haefeli@gmail.com>
2026-05-11 13:55:29 +0200

    profil

--- js/profil.js
+++ js/profil.js
@@ -1,29 +1,36 @@
 // profil.js
-document.getElementById("loginForm").addEventListener("submit", async (e) => {
-  e.preventDefault();
 
-  const email = document.getElementById("email").value.trim();
-  const password = document.getElementById("password").value.trim();
-  const vorname = document.getElementById("vorname").value.trim();
-  const name = document.getElementById("name").value.trim();
+const myID = checkAuth();
 
-  try {
-    const response = await fetch("api/login.php", {
-      method: "POST",
-      // credentials: 'include', // uncomment if front-end & back-end are on different domains
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password, vorname, name }),
-    });
-    const result = await response.json();
+console.log(myID);
+document
+  .getElementById("profilForm")
+  .addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-    if (result.status === "success") {
-      alert("Login successful!");
-      window.location.href = "protected.html";
-    } else {
-      alert(result.message || "Login failed.");
+    const vorname = document.getElementById("vorname").value.trim();
+    const nachname = document.getElementById("nachname").value.trim();
+
+
+
+    try {
+      const response = await fetch("api/profil.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ vorname, nachname }),
+      });
+      const result = await response.json();
+
+      if (result.status === "success") {
+        alert("Registration successful! You can now log in.");
+        window.location.href = "login.html";
+      } else {
+        alert(result.message || "Registration failed.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Something went wrong!");
     }
-  } catch (error) {
-    console.error("Error:", error);
-    alert("Something went wrong!");
-  }
-});
+  });
